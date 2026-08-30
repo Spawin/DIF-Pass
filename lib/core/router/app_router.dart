ed82e54d88ba9bf6ddda8c4b1180e08083299059
Spawin <chissoudifezi228@gmail.com>
2026-08-30 00:07:26 +0000

    Wire the check-in route

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import '../../features/beneficiaries/presentation/screens/beneficiaries_list_screen.dart';
 import '../../features/beneficiaries/presentation/screens/beneficiary_form_screen.dart';
 import '../../features/beneficiaries/presentation/screens/csv_import_screen.dart';
+import '../../features/checkin/presentation/screens/check_in_scan_screen.dart';
 import '../../features/events/presentation/screens/event_archive_screen.dart';
 import '../../features/events/presentation/screens/event_form_screen.dart';
 import '../../features/events/presentation/screens/events_list_screen.dart';
@@ -95,6 +96,16 @@ final appRouter = GoRouter(
       },
       builder: (context, state) => TicketPreviewScreen(
         ticketId: int.parse(state.pathParameters['ticketId']!),
+      ),
+    ),
+    GoRoute(
+      path: '/events/:id/checkin',
+      redirect: (context, state) {
+        final id = int.tryParse(state.pathParameters['id'] ?? '');
+        return id == null ? '/' : null;
+      },
+      builder: (context, state) => CheckInScanScreen(
+        eventId: int.parse(state.pathParameters['id']!),
       ),
     ),
   ],
